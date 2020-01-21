14f3fc932b8fa59ab75407b2b0c29489d5ee1b9f
Dmitriy Bormotov <squier7@gmail.com>
2020-01-21 13:39:00 +0500

    Add timeout commentary

--- conftest.py
+++ conftest.py
@@ -7,6 +7,7 @@ def pytest_addoption(parser):
     parser.addoption("--timeout", action="store", default="3", help="Timeout delay between tests")
 
 
+# It's better to use timeout between tests to not "bomb" market's API with our requests
 @pytest.fixture(autouse=True, scope="function")
 def timeout(request):
     timeout_delay = int(request.config.getoption("timeout"))
